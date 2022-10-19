48de118833071b075fc73f27945309238b2a6f62
Krzysztof <47855045+PiotrowskiKrzysztof@users.noreply.github.com>
2022-10-19 19:28:23 +0200

    add date format

--- src/components/molecules/ContextItem/index.js
+++ src/components/molecules/ContextItem/index.js
@@ -1,22 +1,28 @@
 import { Typography } from "../../atoms/Typography";
 import * as Styled from "./styles";
 
-const ContextItem = ({ title, author, created_at, status }) => (
-  <Styled.Container {...{ status }}>
-    <Styled.InfoContainer>
-      {status === "new" && <Styled.New fontSize="fontSize10">NEW</Styled.New>}
-      <Typography fontSize="fontSize10">{author}</Typography>
-      <Typography fontSize="fontSize10">•</Typography>
-      <Typography fontSize="fontSize10">Dec 17</Typography>
-    </Styled.InfoContainer>
-    <Styled.Title variant="subHeader" {...{ status }}>
-      {title}
-    </Styled.Title>
-    <Styled.Description variant="smallText">
-      Hi Eric, congratulations on completing the previous assignment. This time
-      you will have to focus on...
-    </Styled.Description>
-  </Styled.Container>
-);
+const ContextItem = ({ title, author, created_at, status }) => {
+  const date = new Date(created_at);
+  console.log();
+  return (
+    <Styled.Container {...{ status }}>
+      <Styled.InfoContainer>
+        {status === "new" && <Styled.New fontSize="fontSize10">NEW</Styled.New>}
+        <Typography fontSize="fontSize10">{author}</Typography>
+        <Typography fontSize="fontSize10">•</Typography>
+        <Typography fontSize="fontSize10">{`${date.toLocaleString("en", {
+          month: "short",
+        })} ${date.getDay()}`}</Typography>
+      </Styled.InfoContainer>
+      <Styled.Title variant="subHeader" {...{ status }}>
+        {title}
+      </Styled.Title>
+      <Styled.Description variant="smallText">
+        Hi Eric, congratulations on completing the previous assignment. This
+        time you will have to focus on...
+      </Styled.Description>
+    </Styled.Container>
+  );
+};
 
 export default ContextItem;
